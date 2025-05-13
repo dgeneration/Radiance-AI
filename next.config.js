6bb02593c0db49441380fab0b67f379d674e9233
PATEL JAY DIPAKBHAI <business@dgeneration.io>
2025-05-13 22:53:27 +0530

    Increased Limit

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const withPWA = require('next-pwa')({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
+  maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
   fallbacks: {
     // Add offline fallbacks for specific routes
     document: '/offline', // fallback for document type requests
