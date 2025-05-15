fffbce828ab09733debaaa5da744b222673551e3
PATEL JAY DIPAKBHAI <business@dgeneration.io>
2025-05-15 17:21:09 +0530

    chnages

--- src/app/auth/logout/page.tsx
+++ src/app/auth/logout/page.tsx
@@ -10,8 +10,8 @@ import { Loader2 } from "lucide-react";
 export default function LogoutPage() {
   const router = useRouter();
   const supabase = createBrowserClient(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
 
   useEffect(() => {
